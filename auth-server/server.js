66e295ad2aae6b4f7330ad3dc77cd5cd31e11d9e
etuong <etuong@gmail.com>
2022-11-05 15:43:34 -0700

    Updated auth server

--- auth-server/server.js
+++ auth-server/server.js
@@ -135,7 +135,7 @@ app.post("/logout", (req, res) => {
 
 // Default 404
 app.use((req, res, next) => {
-  res.status(404).send("No such page");
+  res.status(404).send("404 -No such page");
 });
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
